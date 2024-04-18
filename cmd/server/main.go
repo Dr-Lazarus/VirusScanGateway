dbc9a4e136ed6ed82410198f1c66ead083a9f5ef
Visshal Natarajan <visshal_natarajan@mymail.sutd.edu.sg>
2024-04-19 00:02:39 +0800

    feat: Added PROD and DEV env and connection to local PostgreDB

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -8,7 +10,10 @@ import (
 	"text/template"
 
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+var db *sql.DB
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
@@ -30,16 +35,47 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	err := godotenv.Load(".env.dev")
+	env := os.Getenv("APP_ENV")
+	var envFile string
+	if env == "PROD" {
+		envFile = ".env.prod"
+	} else {
+		envFile = ".env.dev"
+	}
+
+	err := godotenv.Load(envFile)
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal("Error loading environment file: ", envFile)
+	}
+
+	var connStr string
+	if env == "PROD" {
+		connStr = fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%s sslmode=require",
+			os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"))
+	} else {
+		connStr = fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable host=localhost",
+			os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+	}
+
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal("Error connecting to the database: ", err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Error pinging the database: ", err)
+	} else {
+		log.Println("Successfully connected to the database.")
 	}
 
 	http.HandleFunc("/", homeHandler)
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		log.Fatal("$PORT must be set")
+		port = "8080"
+		log.Printf("$PORT not set, defaulting to %s in development", port)
 	}
 
 	log.Printf("Server starting on port %s\n", port)
