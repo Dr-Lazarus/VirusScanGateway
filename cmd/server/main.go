5a022c37c7849fb1d49b4a93610e25b36d770120
Visshal Natarajan <visshal_natarajan@mymail.sutd.edu.sg>
2024-04-25 22:13:28 +0800

    feat: Updated Main for separating between Prod and Dev Environments

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/Dr-Lazarus/VirusScanGateway/internal/config"
@@ -21,11 +22,50 @@ func main() {
 	defer dbConn.Close()
 
 	handlers.RegisterRoutes(router, dbConn)
-	port := os.Getenv("PORT")
-	if port == "" {
-		log.Fatal("❌ $PORT not set")
-	}
 
-	log.Printf("🚀 Server starting on port %s\n", port)
-	router.Run(":" + port)
+	env := os.Getenv("ENVIRONMENT")
+
+	if env == "PROD" {
+		httpsPort := os.Getenv("HTTPS_PORT")
+		if httpsPort == "" {
+			httpsPort = "443"
+		}
+
+		sslCert := os.Getenv("SSL_CERT_PATH")
+		sslKey := os.Getenv("SSL_KEY_PATH")
+
+		if sslCert == "" || sslKey == "" {
+			log.Fatal("❌ SSL certificate or key file path not set")
+		}
+
+		go func() {
+			httpPort := os.Getenv("HTTP_PORT")
+			if httpPort == "" {
+				httpPort = "80"
+			}
+
+			log.Printf("🚀 HTTP server starting on port %s for redirect to HTTPS\n", httpPort)
+			httpRouter := gin.Default()
+			// Redirect all HTTP requests to HTTPS
+			httpRouter.GET("/*any", func(c *gin.Context) {
+				target := "https://" + c.Request.Host + c.Request.URL.Path
+				if len(c.Request.URL.RawQuery) > 0 {
+					target += "?" + c.Request.URL.RawQuery
+				}
+				c.Redirect(http.StatusMovedPermanently, target)
+			})
+			log.Fatal(http.ListenAndServe(":"+httpPort, httpRouter))
+		}()
+
+		log.Printf("🚀 HTTPS server starting on port %s\n", httpsPort)
+		log.Fatal(http.ListenAndServeTLS(":"+httpsPort, sslCert, sslKey, router))
+	} else {
+		httpPort := os.Getenv("HTTP_PORT")
+		if httpPort == "" {
+			httpPort = "8080"
+		}
+
+		log.Printf("🚀 HTTP server starting on port %s in development mode\n", httpPort)
+		log.Fatal(http.ListenAndServe(":"+httpPort, router))
+	}
 }
