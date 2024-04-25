44710246b202e40c015ad0edceb411748673880a
Visshal Natarajan <visshal_natarajan@mymail.sutd.edu.sg>
2024-04-25 22:14:59 +0800

    feat: Added Route to Delete

--- internal/handlers/routes.go
+++ internal/handlers/routes.go
@@ -10,9 +10,11 @@ func RegisterRoutes(router *gin.Engine, db *sql.DB) {
 	router.GET("/", homeHandler)
 	router.POST("/upload", func(c *gin.Context) {
 		uploadHandler(c, db)
-		// uploadHandler(c)
 	})
-	router.GET("/getbyID/:SHA256ID", func(c *gin.Context) {
-		getidHandler(c, db)
+	router.GET("/reports/:SHA256ID", func(c *gin.Context) {
+		getReportHandler(c, db)
+	})
+	router.DELETE("/reports/:SHA256ID", func(c *gin.Context) {
+		deleteReportHandler(c, db)
 	})
 }
